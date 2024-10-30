4b03de7e4592e34d7230292f6b12a175b817bbcc
iuncuim <iuncuim@gmail.com>
2024-10-30 21:54:42 +0800

    powersupply: i18n

--- luci/luci-app-powersupply/htdocs/luci-static/resources/view/powersupply/powersupply.js
+++ luci/luci-app-powersupply/htdocs/luci-static/resources/view/powersupply/powersupply.js
@@ -29,35 +29,29 @@ return view.extend({
 	render: function() {
 		var m, s, o;
 
-		m = new form.Map('powersupply', _('Power Supply'));
+		m = new form.Map('powersupply', _('Power Management'));
 
 		s = m.section(form.NamedSection, 'system', _('System Properties'));
 
-		o = s.option(DummyValueExt, 'input_voltage', _('Input Voltage'));
-		o = s.option(DummyValueExt, 'input_voltage_threshold', _('Input Voltage Threshold'));
+		o = s.option(DummyValueExt, 'input_voltage', _('Input power supply voltage, mV'));
 
 		o = s.option(form.ListValue, 'poe_out_power', _('PoE OUT power'));
-		o.default = 'disabled';
 		o.value(0,_('disabled'));
 		o.value(1,_('enabled'));
 
-		o = s.option(form.ListValue, 'poe_out_control', _('PoE OUT control'));
-		o.default = 'auto';
+		o = s.option(form.ListValue, 'poe_out_control', _('Overcurrent protection PoE OUT'));
 		o.value(0,_('disabled'));
-		o.value(1,_('auto'));
+		o.value(1,_('enabled'));
 
 		o = s.option(form.ListValue, 'usb_power', _('USB power'));
-		o.default = 'enabled';
 		o.value(0,_('disabled'));
 		o.value(1,_('enabled'));
 
-		o = s.option(form.ListValue, 'usb_control', _('USB control'));
-		o.default = 'auto';
+		o = s.option(form.ListValue, 'usb_control', _('Overcurrent protection USB'));
 		o.value(0,_('disabled'));
-		o.value(1,_('auto'));
+		o.value(1,_('enabled'));
 
-		o = s.option(form.ListValue, 'ext_power', _('7.5V power'));
-		o.default = 'disabled';
+		o = s.option(form.ListValue, 'ext_power', _('Power output 7.5V'));
 		o.value(0,_('disabled'));
 		o.value(1,_('enabled'));
 
@@ -65,7 +59,6 @@ return view.extend({
 			poll.add(function() {
 				return power_status().then(function(t) {
 					document.getElementById('cbid.powersupply.system.input_voltage').value = t.input_voltage || 'n/a';
-					document.getElementById('cbid.powersupply.system.input_voltage_threshold').value = t.voltage_threshold || 'n/a';
 				});
 			},1);
 
