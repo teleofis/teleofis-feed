0a2b04c14f849ea934c9df341c8804919478aa75
iuncuim <iuncuim@gmail.com>
2025-10-03 22:32:43 +0800

    luci-app-hotplug: i18n

--- luci/luci-app-hotplug/htdocs/luci-static/resources/view/hotplug/iface.js
+++ luci/luci-app-hotplug/htdocs/luci-static/resources/view/hotplug/iface.js
@@ -10,13 +10,7 @@ return view.extend({
 		var m, s, o;
 
 		m = new form.Map('hotplug', _('Interface Rules'));
-		m.description = _('Running commands when the hotplug trigger occurs. <br/>\
-			<br/>\
-			ACTION - ifdown, ifup, ifup-failed, ifupdate, free, reload, iflink, create <br/>\
-			INTERFACE - Name of the logical interface which went up or down (e.g. wan or lan) <br/>\
-			DEVICE - Name of the physical device which went up or down (e.g. eth0 or br-lan or pppoe-wan), when applicable <br/>\
-			<br/>\
-			More information about iface events and variables here: <a href="https://openwrt.org/docs/guide-user/base-system/hotplug#iface">hotplug#iface</a>');
+		m.description = _('Running commands when the hotplug trigger occurs. <br/><br/>ACTION - ifdown, ifup, ifup-failed, ifupdate, free, reload, iflink, create <br/>INTERFACE - Name of the logical interface which went up or down (e.g. wan or lan) <br/>DEVICE - Name of the physical device which went up or down (e.g. eth0 or br-lan or pppoe-wan), when applicable <br/><br/>More information about iface events and variables here: <a href=\"https://openwrt.org/docs/guide-user/base-system/hotplug#iface\">hotplug#iface</a>');
 
 		s = m.section(form.GridSection, 'hotplug');
 		s.addremove = true;
@@ -24,12 +18,12 @@ return view.extend({
 
 		o = s.option(form.TextValue, 'description', _('Description'));
 
-		o = s.option(form.Flag, 'enabled', _('Enabled'));
+		o = s.option(form.Flag, 'enabled', _('Activate'));
 		o.rmempty = false;
 		o.editable = true;
 
-		o = s.option(form.ListValue, 'action', _('$ACTION'), _('Hotplug event'));
-		o.default = "mode1";
+		o = s.option(form.ListValue, 'action', _('Variable $ACTION'), _('The type of event that will trigger the command'));
+		o.default = "ifup";
 		o.value("ifup", _("iface up (ifup)"));
 		o.value("ifdown", _("iface down (ifdown)"));
 		o.value("ifup-failed", _("iface up failed (ifup-failed)"));
@@ -39,13 +33,14 @@ return view.extend({
 		o.value("iflink", _("Iface received link (iflink)"));
 		o.value("create", _("iface created (create)"));
 
-		o = s.option(widgets.NetworkSelect, 'interface', _('$INTERFACE'), _('Interface'));
+		o = s.option(widgets.NetworkSelect, 'interface', _('Variable $INTERFACE'), _('The name of the logical interface to which the command will be applied'));
+		o.default = "internet";
 		o.rmempty = false;
 		o.textvalue = function(section_id) {
 			return uci.get('hotplug', section_id, 'interface');
 		}
 
-		o = s.option(form.Value, 'command', _('Command to execute'));
+		o = s.option(form.DynamicList, 'command', _('Command to execute'));
 
 		return m.render();
 	}
